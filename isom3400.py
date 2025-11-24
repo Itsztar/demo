74de8fa1892f903b01cf61df40fc9df534669971
Itsztar <135846454+Itsztar@users.noreply.github.com>
2025-11-24 14:28:28 +0800

    Update isom3400.py

--- isom3400.py
+++ isom3400.py
@@ -13,4 +13,4 @@ st.data_editor(df)              # Editable table
 st.table(df)                    # Static table
 
 # Customize columns directly in the dataframe display
-st.dataframe(df.style.format({'Sales': '${:,.2f}', 'Customers': 'FUCK{:,.2f}'}))
+st.dataframe(df.style.format({'Sales': '${:,.2f}', 'Customers': '{:,.0f}cc'}))
