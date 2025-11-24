cd6d4ee32e038a0fe4904f9a41a1e63e1e58baf7
Itsztar <135846454+Itsztar@users.noreply.github.com>
2025-11-24 13:47:40 +0800

    Update isom3400.py

--- isom3400.py
+++ isom3400.py
@@ -12,7 +12,7 @@ with st.sidebar:
                  "2-circle-fill",
                  "3-circle-fill"],
         menu_icon= "emoji-smile-fill",
-        default_index=1,
+        default_index=0,
     )
 
 if selected == "Home":
